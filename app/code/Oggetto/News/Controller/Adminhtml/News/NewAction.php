380c8badb3627e2e61b54f34a3ea1d9e0037a25b
Kirill Vnukov <kirillgrandson@gmail.com>
2022-11-10 12:08:18 +0300

    OGGYKIDS-206875: Add action to add news

---
+++ app/code/Oggetto/News/Controller/Adminhtml/News/NewAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Oggetto\News\Controller\Adminhtml\News;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class NewAction extends Action implements HttpGetActionInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
