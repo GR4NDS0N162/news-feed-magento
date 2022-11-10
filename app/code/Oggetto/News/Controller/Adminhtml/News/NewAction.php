9c238b1f6bba11404c0e4577990a1bc902849f54
Kirill Vnukov <kirillgrandson@gmail.com>
2022-11-10 12:58:00 +0300

    OGGYKIDS-206875: Implement dependency on PageFactory class

--- app/code/Oggetto/News/Controller/Adminhtml/News/NewAction.php
+++ app/code/Oggetto/News/Controller/Adminhtml/News/NewAction.php
@@ -3,10 +3,28 @@
 namespace Oggetto\News\Controller\Adminhtml\News;
 
 use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
 
 class NewAction extends Action implements HttpGetActionInterface
 {
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
 
     /**
      * @inheritDoc
