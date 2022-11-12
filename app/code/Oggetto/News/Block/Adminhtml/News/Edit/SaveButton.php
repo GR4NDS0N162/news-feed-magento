84dd4eb989dcc8d9a7d9a66b06c6845d426713c9
Kirill Vnukov <kirillgrandson@gmail.com>
2022-11-12 16:36:46 +0300

    OGGYKIDS-206875: Add entity saving options

--- app/code/Oggetto/News/Block/Adminhtml/News/Edit/SaveButton.php
+++ app/code/Oggetto/News/Block/Adminhtml/News/Edit/SaveButton.php
@@ -5,29 +5,94 @@ namespace Oggetto\News\Block\Adminhtml\News\Edit;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 use Magento\Ui\Component\Control\Container;
 
-class SaveButton implements ButtonProviderInterface
+class SaveButton extends GenericButton implements ButtonProviderInterface
 {
     /**
-     * @inheritDoc
+     * Get button data
+     *
+     * @return array
      */
     public function getButtonData()
     {
         return [
-            'label'          => __('Save'),
-            'class'          => 'save primary',
-            'data_attribute' => [
+            'label'                      => __('Save'),
+            'class'                      => 'save primary',
+            'data_attribute'             => [
                 'mage-init' => [
                     'buttonAdapter' => [
                         'actions' => [
                             [
                                 'targetName' => 'news_news_form.news_news_form',
                                 'actionName' => 'save',
+                                'params'     => [
+                                    true,
+                                    [
+                                        'back' => 'continue',
+                                    ],
+                                ],
                             ],
                         ],
                     ],
                 ],
             ],
-            'class_name'     => Container::DEFAULT_CONTROL,
+            'class_name'                 => Container::SPLIT_BUTTON,
+            'options'                    => $this->getOptions(),
+            'dropdown_button_aria_label' => __('Save options'),
+        ];
+    }
+
+    /**
+     * Retrieve options
+     *
+     * @return array
+     */
+    private function getOptions()
+    {
+        return [
+            [
+                'label'          => __('Save & Duplicate'),
+                'id_hard'        => 'save_and_duplicate',
+                'data_attribute' => [
+                    'mage-init' => [
+                        'buttonAdapter' => [
+                            'actions' => [
+                                [
+                                    'targetName' => 'news_news_form.news_news_form',
+                                    'actionName' => 'save',
+                                    'params'     => [
+                                        true,
+                                        [
+                                            'back' => 'duplicate',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                'id_hard'        => 'save_and_close',
+                'label'          => __('Save & Close'),
+                'data_attribute' => [
+                    'mage-init' => [
+                        'buttonAdapter' => [
+                            'actions' => [
+                                [
+                                    'targetName' => 'news_news_form.news_news_form',
+                                    'actionName' => 'save',
+                                    'params'     => [
+                                        true,
+                                        [
+                                            'back' => 'close',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
         ];
     }
 }
