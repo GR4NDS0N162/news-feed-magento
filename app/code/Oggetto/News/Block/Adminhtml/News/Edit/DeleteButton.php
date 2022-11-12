80d7e658a27e5a00259e35b9cb1090055588e504
Kirill Vnukov <kirillgrandson@gmail.com>
2022-11-12 17:18:43 +0300

    OGGYKIDS-206875: Add delete button

---
+++ app/code/Oggetto/News/Block/Adminhtml/News/Edit/DeleteButton.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Oggetto\News\Block\Adminhtml\News\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Oggetto\News\Api\Data\NewsInterface;
+
+class DeleteButton extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getButtonData()
+    {
+        $data = [];
+        if ($this->getNewsId()) {
+            $data = [
+                'label'      => __('Delete News'),
+                'class'      => 'delete',
+                'on_click'   => 'deleteConfirm(\'' . __(
+                    'Are you sure you want to do this?',
+                ) . '\', \'' . $this->getDeleteUrl() . '\', {"data": {}})',
+                'sort_order' => 20,
+            ];
+        }
+        return $data;
+    }
+
+    /**
+     * URL to send delete requests to.
+     *
+     * @return string
+     */
+    public function getDeleteUrl()
+    {
+        return $this->getUrl('*/*/delete', [NewsInterface::ID => $this->getNewsId()]);
+    }
+}
