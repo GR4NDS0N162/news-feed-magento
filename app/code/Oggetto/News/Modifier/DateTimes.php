48e6d8ea61d60b5ddf2ce7985d90b729b0ed80db
Kirill Vnukov <kirillgrandson@gmail.com>
2022-11-14 17:23:19 +0300

    OGGYKIDS-206875: Add fields via the modifier

--- app/code/Oggetto/News/Modifier/DateTimes.php
+++ app/code/Oggetto/News/Modifier/DateTimes.php
@@ -2,6 +2,7 @@
 
 namespace Oggetto\News\Modifier;
 
+use Magento\Ui\Component\Form\Field;
 use Magento\Ui\DataProvider\Modifier\ModifierInterface;
 
 class DateTimes implements ModifierInterface
@@ -19,6 +20,39 @@ class DateTimes implements ModifierInterface
      */
     public function modifyMeta(array $meta)
     {
+        $meta['general'] = [
+            'children' => [
+                'creation_time' => [
+                    'arguments' => [
+                        'data' => [
+                            'config' => [
+                                'formElement'   => 'input',
+                                'componentType' => Field::NAME,
+                                'label'         => __('News Creation Time'),
+                                'visible'       => 1,
+                                'required'      => 1,
+                                'disabled'      => true,
+                            ],
+                        ],
+                    ],
+                ],
+                'update_time'   => [
+                    'arguments' => [
+                        'data' => [
+                            'config' => [
+                                'formElement'   => 'input',
+                                'componentType' => Field::NAME,
+                                'label'         => __('News Modification Time'),
+                                'visible'       => 1,
+                                'required'      => 1,
+                                'disabled'      => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
         return $meta;
     }
 }
