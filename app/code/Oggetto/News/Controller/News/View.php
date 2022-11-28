063d0da90eee0977ea83ebf44b0f761ce2964bb5
Kirill Vnukov <kirillgrandson@gmail.com>
2022-11-28 21:34:32 +0300

    OGGYKIDS-207801: Handle NoSuchEntityException

--- app/code/Oggetto/News/Controller/News/View.php
+++ app/code/Oggetto/News/Controller/News/View.php
@@ -6,6 +6,7 @@ namespace Oggetto\News\Controller\News;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Result\PageFactory;
@@ -19,6 +20,11 @@ class View implements HttpGetActionInterface
      * @var PageFactory
      */
     protected PageFactory $resultPageFactory;
+
+    /**
+     * @var RedirectFactory
+     */
+    protected RedirectFactory $resultRedirectFactory;
 
     /**
      * @var NewsRepositoryInterface
@@ -37,17 +43,20 @@ class View implements HttpGetActionInterface
 
     /**
      * @param PageFactory $resultPageFactory
+     * @param RedirectFactory $resultRedirectFactory
      * @param NewsRepositoryInterface $newsRepository
      * @param RequestInterface $request
      * @param UrlInterface $url
      */
     public function __construct(
         PageFactory $resultPageFactory,
+        RedirectFactory $resultRedirectFactory,
         NewsRepositoryInterface $newsRepository,
         RequestInterface $request,
         UrlInterface $url,
     ) {
         $this->resultPageFactory = $resultPageFactory;
+        $this->resultRedirectFactory = $resultRedirectFactory;
         $this->newsRepository = $newsRepository;
         $this->request = $request;
         $this->url = $url;
@@ -55,13 +64,16 @@ class View implements HttpGetActionInterface
 
     /**
      * @inheritDoc
-     *
-     * @throws NoSuchEntityException
      */
     public function execute()
     {
+        $resultRedirect = $this->resultRedirectFactory->create();
         $newsId = $this->request->getParam(NewsInterface::ID);
-        $news = $this->newsRepository->getById($newsId);
+        try {
+            $news = $this->newsRepository->getById($newsId);
+        } catch (NoSuchEntityException) {
+            return $resultRedirect->setPath('*/*/');
+        }
 
         $resultPage = $this->resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->set($news->getTitle());
