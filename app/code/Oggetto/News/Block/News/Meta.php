781afe88c3b64d26cee5d42f50189c4ed0eec1ec
Kirill Vnukov <kirillgrandson@gmail.com>
2022-12-05 17:39:02 +0300

    OGGYKIDS-208206: Render metadata

--- app/code/Oggetto/News/Block/News/Meta.php
+++ app/code/Oggetto/News/Block/News/Meta.php
@@ -6,14 +6,11 @@ namespace Oggetto\News\Block\News;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Template;
-use Magento\Framework\View\Element\Template\Context;
 use Oggetto\News\Api\Data\NewsInterface;
 use Oggetto\News\Api\NewsRepositoryInterface;
 use Oggetto\News\Model\NewsFactory;
-use Zend_Filter_Exception as FilterException;
-use Zend_Filter_Interface as FilterInterface;
 
-class View extends Template
+class Meta extends Template
 {
     /**
      * @var NewsRepositoryInterface
@@ -26,32 +23,24 @@ class View extends Template
     protected NewsFactory $newsFactory;
 
     /**
-     * @var FilterInterface
-     */
-    protected FilterInterface $templateProcessor;
-
-    /**
      * @var NewsInterface
      */
     protected NewsInterface $news;
 
     /**
-     * @param Context                 $context
+     * @param Template\Context        $context
      * @param NewsRepositoryInterface $newsRepository
      * @param NewsFactory             $newsFactory
-     * @param FilterInterface         $templateProcessor
      * @param array                   $data
      */
     public function __construct(
         Template\Context $context,
         NewsRepositoryInterface $newsRepository,
         NewsFactory $newsFactory,
-        FilterInterface $templateProcessor,
         array $data = [],
     ) {
         $this->newsRepository = $newsRepository;
         $this->newsFactory = $newsFactory;
-        $this->templateProcessor = $templateProcessor;
         parent::__construct($context, $data);
     }
 
@@ -74,17 +63,32 @@ class View extends Template
     }
 
     /**
-     * Get news content
+     * Get news meta title
      *
      * @return string
      */
-    public function getNewsContent(): string
+    public function getNewsMetaTitle(): string
     {
-        $content = $this->news->getContent() ?? '';
-        try {
-            return $this->templateProcessor->filter($content);
-        } catch (FilterException) {
-            return '';
-        }
+        return $this->news->getMetaTitle() ?? '';
+    }
+
+    /**
+     * Get news meta keywords
+     *
+     * @return string
+     */
+    public function getNewsMetaKeywords(): string
+    {
+        return $this->news->getMetaKeywords() ?? '';
+    }
+
+    /**
+     * Get news meta description
+     *
+     * @return string
+     */
+    public function getNewsMetaDescription(): string
+    {
+        return $this->news->getMetaDescription() ?? '';
     }
 }
