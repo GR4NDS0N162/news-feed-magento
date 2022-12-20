a41625783cbb603230738fe052e273d832d5935c
Kirill Vnukov <kirillgrandson@gmail.com>
2022-12-20 16:01:19 +0300

    OGGYKIDS-208736: Remove unused attributes to select

--- app/code/Oggetto/News/Block/News/Product/Related.php
+++ app/code/Oggetto/News/Block/News/Product/Related.php
@@ -149,10 +149,6 @@ class Related extends ListProduct
         )->addAttributeToSelect(
             'sku'
         )->addAttributeToSelect(
-            'visibility'
-        )->addAttributeToSelect(
-            'status'
-        )->addAttributeToSelect(
             'price'
         )->joinField(
             'position',
