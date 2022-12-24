42e8322c90f60fa2b23038d4e961c6298508095c
Kirill Vnukov <kirillgrandson@gmail.com>
2022-12-24 23:08:29 +0300

    OGGYKIDS-209111: Implement execute() method of save extension class

--- app/code/Oggetto/News/Model/ResourceModel/News/Relation/Store/SaveHandler.php
+++ app/code/Oggetto/News/Model/ResourceModel/News/Relation/Store/SaveHandler.php
@@ -6,6 +6,7 @@ namespace Oggetto\News\Model\ResourceModel\News\Relation\Store;
 
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\EntityManager\Operation\ExtensionInterface;
+use Oggetto\News\Api\Data\NewsInterface;
 use Oggetto\News\Model\ResourceModel\News;
 
 class SaveHandler implements ExtensionInterface
@@ -35,9 +36,39 @@ class SaveHandler implements ExtensionInterface
     /**
      * @inheritDoc
      */
-    public function execute($entity, $arguments = [])
+    public function execute($news, $arguments = [])
     {
-        // TODO: Implement execute() method.
-        return $entity;
+        if (!($news instanceof NewsInterface)) {
+            return $news;
+        }
+
+        $connection = $this->resourceNews->getConnection();
+        $newsId = $news->getId();
+
+        $oldStores = $this->resourceNews->lookupStoreIds((int) $newsId);
+        $newStores = (array) $news->getData(NewsInterface::STORES);
+
+        $table = $this->resourceNews->getTable(News::NEWS_STORE_TABLE_NAME);
+
+        $delete = array_diff($oldStores, $newStores);
+        if ($delete) {
+            $where = $connection->prepareSqlCondition(News::NEWS_ID, ['eq' => $newsId]);
+            $where .= ' AND ' . $connection->prepareSqlCondition(News::STORE_ID, ['in' => $delete]);
+            $connection->delete($table, $where);
+        }
+
+        $insert = array_diff($newStores, $oldStores);
+        if ($insert) {
+            $data = [];
+            foreach ($insert as $storeId) {
+                $data[] = [
+                    News::NEWS_ID  => (int) $newsId,
+                    News::STORE_ID => (int) $storeId,
+                ];
+            }
+            $connection->insertMultiple($table, $data);
+        }
+
+        return $news;
     }
 }
