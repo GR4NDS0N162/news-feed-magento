58736e6f7e7908204d6cb32b2b51b2ac9d0f62e3
Kirill Vnukov <kirillgrandson@gmail.com>
2022-12-25 01:14:27 +0300

    OGGYKIDS-209111: Update form data provider

--- app/code/Oggetto/News/Model/News/DataProvider.php
+++ app/code/Oggetto/News/Model/News/DataProvider.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Oggetto\News\Model\News;
 
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Ui\DataProvider\Modifier\PoolInterface;
 use Magento\Ui\DataProvider\ModifierPoolDataProvider;
-use Oggetto\News\Model\News;
+use Oggetto\News\Api\Data\NewsInterface;
+use Oggetto\News\Api\NewsRepositoryInterface;
+use Oggetto\News\Model\NewsFactory;
 use Oggetto\News\Model\ResourceModel\News\Collection as NewsCollection;
 use Oggetto\News\Model\ResourceModel\News\CollectionFactory;
 
@@ -23,24 +27,48 @@ class DataProvider extends ModifierPoolDataProvider
     protected $loadedData;
 
     /**
-     * @param string                 $name
-     * @param string                 $primaryFieldName
-     * @param string                 $requestFieldName
-     * @param CollectionFactory      $newsCollectionFactory
-     * @param array                  $meta
-     * @param array                  $data
-     * @param PoolInterface|null     $pool
+     * @var RequestInterface
+     */
+    protected RequestInterface $request;
+
+    /**
+     * @var NewsFactory
+     */
+    protected NewsFactory $newsFactory;
+
+    /**
+     * @var NewsRepositoryInterface
+     */
+    protected NewsRepositoryInterface $newsRepository;
+
+    /**
+     * @param string                  $name
+     * @param string                  $primaryFieldName
+     * @param string                  $requestFieldName
+     * @param CollectionFactory       $newsCollectionFactory
+     * @param RequestInterface        $request
+     * @param NewsFactory             $newsFactory
+     * @param NewsRepositoryInterface $newsRepository
+     * @param array                   $meta
+     * @param array                   $data
+     * @param PoolInterface|null      $pool
      */
     public function __construct(
         $name,
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $newsCollectionFactory,
+        RequestInterface $request,
+        NewsFactory $newsFactory,
+        NewsRepositoryInterface $newsRepository,
         array $meta = [],
         array $data = [],
         PoolInterface $pool = null,
     ) {
         $this->collection = $newsCollectionFactory->create();
+        $this->request = $request;
+        $this->newsFactory = $newsFactory;
+        $this->newsRepository = $newsRepository;
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data, $pool);
     }
 
@@ -52,18 +80,25 @@ class DataProvider extends ModifierPoolDataProvider
         if (isset($this->loadedData)) {
             return $this->loadedData;
         }
-        $items = $this->collection->getItems();
-        /** @var News $news */
-        foreach ($items as $news) {
-            $this->loadedData[$news->getId()] = $news->getData();
-        }
 
-        if (!empty($data)) {
-            $news = $this->collection->getNewEmptyItem();
-            $news->setData($data);
-            $this->loadedData[$news->getId()] = $news->getData();
-        }
+        $news = $this->getCurrentNews();
+        $this->loadedData[$news->getId()] = $news->getData();
 
         return $this->loadedData;
+    }
+
+    private function getCurrentNews(): NewsInterface
+    {
+        $news = $this->newsFactory->create();
+
+        if ($newsId = $this->request->getParam(NewsInterface::ID)) {
+            try {
+                $news = $this->newsRepository->getById($newsId);
+            } catch (NoSuchEntityException) {
+                $news = $this->newsFactory->create();
+            }
+        }
+
+        return $news;
     }
 }
