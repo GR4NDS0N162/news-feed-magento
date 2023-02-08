0e8e699d245674cd7b49796d54ae4df22b4241a4
Kirill Vnukov <kirillgrandson@gmail.com>
2023-02-08 18:16:13 +0300

    OGGYKIDS-210345: Add product attribute "Is Best"

---
+++ app/code/Oggetto/BestCheckout/Model/Config/Source/YesNoMaybe.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oggetto\BestCheckout\Model\Config\Source;
+
+use Magento\Eav\Model\Entity\Attribute\Source\AbstractSource;
+
+class YesNoMaybe extends AbstractSource
+{
+    public const VALUE_YES = 1;
+    public const VALUE_NO = 0;
+    public const VALUE_MAYBE = 2;
+
+    /**
+     * @inheritDoc
+     */
+    public function getAllOptions(): array
+    {
+        if ($this->_options === null) {
+            $this->_options = [
+                ['label' => __('Yes'), 'value' => self::VALUE_YES],
+                ['label' => __('No'), 'value' => self::VALUE_NO],
+                ['label' => __('Maybe'), 'value' => self::VALUE_MAYBE],
+            ];
+        }
+        return $this->_options;
+    }
+}
