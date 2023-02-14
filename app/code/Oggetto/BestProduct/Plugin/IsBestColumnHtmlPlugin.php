2dfd890acf12d35c20978c0cfb4b4b2b652bdd80
Kirill Vnukov <kirillgrandson@gmail.com>
2023-02-14 13:31:52 +0300

    OGGYKIDS-210345: Implement getting the attribute value for the is-best column

---
+++ app/code/Oggetto/BestProduct/Plugin/IsBestColumnHtmlPlugin.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oggetto\BestProduct\Plugin;
+
+use Magento\Framework\DataObject;
+use Magento\Sales\Block\Adminhtml\Order\View\Items\Renderer\DefaultRenderer;
+use Oggetto\BestProduct\Model\Config\Source\YesNoMaybe;
+use Oggetto\BestProduct\Setup\Patch\Data\IsBestOrderItemPatch;
+
+class IsBestColumnHtmlPlugin
+{
+    /**
+     * @var YesNoMaybe
+     */
+    private YesNoMaybe $source;
+
+    /**
+     * @param YesNoMaybe $source
+     */
+    public function __construct(
+        YesNoMaybe $source,
+    ) {
+        $this->source = $source;
+    }
+
+    /**
+     * @param DefaultRenderer $subject
+     * @param string          $result
+     * @param DataObject      $item
+     * @param string          $column
+     * @param null            $field
+     * @return string
+     */
+    public function afterGetColumnHtml(
+        DefaultRenderer $subject,
+        $result,
+        DataObject $item,
+        $column,
+    ): string {
+        if ($column == 'is-best') {
+            $html = $this->source->getOptionText(
+                $item->getData(IsBestOrderItemPatch::ATTRIBUTE_CODE)
+            );
+        } else {
+            $html = $result;
+        }
+        return (string) $html;
+    }
+}
