98a1295b4beaff27c73359f9f429f2eb0b3a3218
Kirill Vnukov <kirillgrandson@gmail.com>
2023-02-16 12:10:13 +0300

    OGGYKIDS-210345: Add a JS mixin to modify data submission

---
+++ app/code/Oggetto/ComfortDeliveryTime/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+let config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/set-shipping-information': {
+                'Oggetto_ComfortDeliveryTime/js/action/set-shipping-information-mixin': true
+            }
+        }
+    }
+};
